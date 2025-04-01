590139a7bab8562ed264653ababd61bb2f3a47da
Leandro7-moreira <leandro-denti@hotmail.com>
2025-03-31 22:03:05 -0300

    atualização html/ccs/js conta 1.0

--- js/conta.js
+++ js/conta.js
@@ -5,11 +5,13 @@ document.getElementById('cadastroForm').addEventListener('submit', function (e) 
     const nome = document.getElementById('nome').value;
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
+    const vencimentoCartao = document.getElementById('vencimentoCartao').value; // Captura o vencimento do cartão
 
     const dadosCadastro = {
         nome: nome,
         email: email,
-        senha: senha
+        senha: senha,
+        vencimentoCartao: vencimentoCartao // Adiciona o vencimento do cartão
     };
 
     // Envia os dados para o backend Flask
@@ -30,18 +32,18 @@ document.getElementById('cadastroForm').addEventListener('submit', function (e) 
     });
 });
 
-// Formulário de Alteração de Senha
+// Formulário Criar Senha
 document.getElementById('senhaForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const novaSenha = document.getElementById('novaSenha').value;
+    const senha = document.getElementById('senha').value;
     const confirmarSenha = document.getElementById('confirmarSenha').value;
     const email = document.getElementById('email').value;  // Supondo que o email também seja enviado
 
     // Verifica se as senhas são iguais
-    if (novaSenha === confirmarSenha) {
+    if (senha === confirmarSenha) {
         const dadosSenha = {
-            novaSenha: novaSenha,
+            senha: senha,
             email: email
         };
 
@@ -64,4 +66,34 @@ document.getElementById('senhaForm').addEventListener('submit', function (e) {
     } else {
         alert('As senhas não coincidem!');
     }
+});
+
+// Formulário de Endereço
+document.getElementById('enderecoForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const cidade = document.getElementById('cidade').value;
+    const complemento = document.getElementById('complemento').value;
+
+    const dadosEndereco = {
+        cidade: cidade,
+        complemento: complemento
+    };
+
+    // Envia os dados para o backend Flask
+    fetch('/salvar_endereco', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dadosEndereco)
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);  // Exibe a resposta do backend (sucesso ou erro)
+    })
+    .catch(error => {
+        console.error('Erro:', error);  // Trata qualquer erro
+        alert('Ocorreu um erro ao salvar o endereço.');
+    });
 });
